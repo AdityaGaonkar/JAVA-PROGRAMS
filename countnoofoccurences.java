112ef77ae575f54e3232f5844da657ad4a93724f
Adi <gaonkar251@gmail.com>
2023-03-04 18:03:09 +0530

    added first commit

---
+++ countnoofoccurences.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class countnoofoccurences {
+    static int countOccurences(int[] arr,int ele){
+        int count=0;
+        for (int i=0;i< arr.length;i++){
+            if (ele==arr[i]){
+                count++;
+            }
+        }
+        return count;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("enter the elements of the array");
+        for (int i=0;i< arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("entered elements are");
+        for (int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        System.out.println("enter the ele to be searched");
+        int ele= sc.nextInt();
+        //countOccurences(arr,ele);
+        System.out.println("element found "+ countOccurences(arr,ele)+" times");
+    }
+}
