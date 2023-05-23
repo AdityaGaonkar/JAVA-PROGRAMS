73eed09ce3c949edc5806d49bc534791e8771fc1
Adi <gaonkaraditya49@gmail.com>
2023-05-23 18:02:34 +0530

    20th commit

---
+++ SELECTION_SORT.java
@@ -0,0 +1,23 @@
+public class SELECTION_SORT {
+    static void selectionsort(int[] arr){
+        int n= arr.length;
+        for (int i=0;i<n-1;i++){
+            int min_index=i;
+            for (int j=i+1;j<n;j++){
+                if (arr[j]<arr[min_index]){
+                    min_index=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min_index];
+            arr[min_index]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={6,58,4,9,5,2,1,156,250};
+        selectionsort(arr);
+        for(int i:arr){
+            System.out.println(i+ " ");
+        }
+    }
+}
