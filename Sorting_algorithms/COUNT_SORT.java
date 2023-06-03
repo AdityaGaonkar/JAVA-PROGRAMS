ea6e1d17552d22d8ff7de61b0a8f7e397c15c3ae
Adi <gaonkaraditya49@gmail.com>
2023-06-03 10:20:51 +0530

    commit

--- Sorting_algorithms/COUNT_SORT.java
+++ Sorting_algorithms/COUNT_SORT.java
@@ -15,6 +15,9 @@ public class COUNT_SORT{
         }
         return max;
     }
+    /*
+    its unstable algorithm
+
     static void countsort(int[] arr){
         int max=findmax(arr);
         int[] count=new int[max+1];
@@ -28,11 +31,35 @@ public class COUNT_SORT{
             }
         }
     }
+     */
+    static void stableCountSort(int[] arr){
+        int max=findmax(arr);
+        int[] count=new int[max+1];
+        for (int i=0;i< arr.length;i++){
+            count[arr[i]]++;
+        }
+        int n= arr.length;
+        int[] output=new int[n];
+        //make prefix sum of count array
+        for (int i=1;i< count.length;i++){
+            count[i]+=count[i-1];
+        }
+        for (int i=n-1;i>=0;i--){
+            int index=count[arr[i]]-1;
+            output[index]=arr[i];
+            count[arr[i]]--;
+        }
+        for (int i=0;i<n;i++){
+            arr[i]=output[i];
+        }
+
+    }
     public static void main(String[] args) {
         int[] arr={4,1,9,5,6,7,2,15,16,14,11};
         System.out.println("before sorting");
         display(arr);
-        countsort(arr);
+        //countsort(arr);
+        stableCountSort(arr);
         System.out.println("\nafter sorting");
         display(arr);
     }
